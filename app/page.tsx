76f3058c289571196548e808529d164a7470ed0c
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2025-01-06 21:08:33 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="w-full h-dvh bg-bg-page-950">
+        <div>
+          <p className="text-font-primary-200">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
+            beatae.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
